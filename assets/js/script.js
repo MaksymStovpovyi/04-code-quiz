01e18beca2e784491fb8ebc621b8fecb4c25ac35
maxSt <stovpovymax@gmail.com>
2023-01-22 18:59:18 -0500

    v0.4 need fix timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ let elemTime = document.getElementById('time');
 
 let className = 'class ="cnt_title"';
 let pageIs = 'start';
-let totalTime = 30;
+let totalTime = 3;
 let leftTime = totalTime;
 let finalScore;
 
@@ -96,11 +96,7 @@ function runPage (page) {
                 let yourScore = document.getElementById('yourScore');
                 yourScore.innerHTML = leftTime;
 
-
-                console.log('finalScore ' + finalScore);
-
             } else if (leftTime >= 0) {
-                console.log(leftTime);
                 elemTime.innerHTML = leftTime;
                 --leftTime;
 
@@ -110,12 +106,6 @@ function runPage (page) {
             }   
         }, 1000);
     }
-
-    //HiScore
-    if (pageIs == 'hiScore') {
-///// ----> STOP HERE - creat li for <ul></ul>
-    }
-
 
     //Listener for buttons 
     let btn = document.getElementsByClassName('btn');
@@ -162,38 +152,54 @@ function runPage (page) {
                 
 
             } else if (pageIs === 'allDone') {
-
+                // name from form
                 let form = document.forms.initials;
                 let elem = form.elements.text;
-                let fromForm = [[elem.value, finalScore]];
 
-                console.log(elem.value + " " + finalScore);
+                // array [name and score]
+                let fromForm = [elem.value, finalScore];
                 
                 if (localStorage.hiScore) {
-
                     let fromLS = JSON.parse(localStorage.getItem('hiScore'));
-
                     fromLS.unshift(fromForm);
-
                     localStorage.setItem('hiScore', JSON.stringify(fromLS));
-
                 } else {
-                    localStorage.setItem('hiScore', JSON.stringify(fromForm));
+                    localStorage.setItem('hiScore', JSON.stringify([fromForm]));
                 }
 
                 pageIs = "hiScore";
                 runPage(hiScore);
 
+                //  create score list
+                if (pageIs == 'hiScore') {
+                        let ul = document.getElementById('score-list');
+                        let arrHiScore = JSON.parse(localStorage.getItem('hiScore'));
+
+                        for (let i = 0; i < arrHiScore.length; i++) {
+                            let li = document.createElement("li"); 
+                            ul.appendChild(li);
+                            li.textContent = `${arrHiScore[i][0]}:  ${arrHiScore[i][1]}`;
+                            
+                            if (i == 19) {
+                                break;
+                            }
+                        }
+                    }
+
             } else if (pageIs === 'hiScore') {
 
-                leftTime = totalTime;
-                pageIs = "start";
-                runPage(pageStart);
+                //Clear score list
+                if (e.target.textContent == 'Clear score' && localStorage.hiScore) {
+                    let ul = document.getElementById('score-list').remove();
+                    localStorage.clear();
+                } else if (e.target.textContent == 'Go back') {
+                    leftTime = totalTime;
+                    pageIs = "start";
+                    runPage(pageStart);
+                }
             }
-
         })
     }   
-    
 }
 
 // DROP
